06113f8783ab28d013c50fc6d0fa0d6e6846b75a
SRchris <100946291+SRchris@users.noreply.github.com>
2023-06-02 16:57:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,20 +9,31 @@ def get_data():
     }
     response = requests.request("GET", url, headers=headers)
     data = response.json()
+
+    # Fetch APR data for each operator
+    for item in data['data']:
+        apr_url = f"https://api.rated.network/v0/eth/operators/{item['id']}/apr?window=1d&idType=entity&size=1000"
+        apr_response = requests.request("GET", apr_url, headers=headers)
+        apr_data = apr_response.json()
+        item['apr'] = apr_data['percentage']
+
     return data
 
 def create_dataframe(data):
     lst_name = []
     validator_count = []
     rating = []
+    apr = []
     for item in data['data']:
         lst_name.append(item['id'])
         validator_count.append(item['validatorCount'])
         rating.append(item['avgValidatorEffectiveness'])
+        apr.append(item['apr'])
     df = pd.DataFrame({
         'LST name': lst_name,
         'Validator Count': validator_count,
-        'Rating': rating
+        'Rating': rating,
+        'APR 1d': apr
     })
     return df
 
